b3fe6cef1be140a8a454b334294b7f5c05e1db47
zane pemberton <zanepemberton@outlook.com>
2020-09-10 23:25:37 +1000

    removed un-needed code

--- script.js
+++ script.js
@@ -1,79 +1,45 @@
-$(function () {});
-  
-/* Declare Variables */
-var today = moment().format("dddd, MMMM Do");
+$(document).ready(function(){
 
-var now = moment().format("H A");
+    var hour = moment().hours();
 
-/* planWorkday entries for each hour of the workday */
-var planWorkday = [
-  { time: "9 AM", event: "" },
-  { time: "10 AM", event: "" },
-  { time: "11 AM", event: "" },
-  { time: "12 PM", event: "" },
-  { time: "1 PM", event: "" },
-  { time: "2 PM", event: "" },
-  { time: "3 PM", event: "" },
-  { time: "4 PM", event: "" },
-  { time: "5 PM", event: "" },
-];
+    //Moment.js date for top of page
+    function getDate(){
+        $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    };
 
-/* Local Storage check */
-var workEvents = JSON.parse(localStorage.getItem("workDay"));
-if (workEvents) {
-  planWorkday = workEvents;
-}
+    //Color coding schedule dependant upon hour
+    function colorSchedule(){
+        $("input").each(function(){
+            var rowHour = $(this).attr("id");
+            var rowNumber = parseInt(rowHour);
+            if (rowNumber === hour){
+                $(this).addClass("present");
+            } else if (rowNumber < hour){
+                $(this).addClass("past");
+            } else {
+                $(this).addClass("future");
+            };
+        });
+    };
 
-/* Current Day */
-$("#currentDay").text(today);
+    //Rendering stored inputs after page refresh
+    function renderStoredInputs(){
+        $(".event").each(function(){
+            var inputId = $(this).attr("id");
+            $(this).val(localStorage.getItem(inputId));
+        });
+    };
 
-/* Create rows */
-planWorkday.forEach(function(timeBlock, index) {
-	var timeLabel = timeBlock.time;
-	var blockColor = colorRow(timeLabel);
-	var row =
-		'<div class="time-block" id="' +
-		index +
-		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-		timeLabel +
-		'</div><textarea class="form-control ' +
-		blockColor +
-		'">' +
-		timeBlock.event +
-		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+    //Click event to save user input in local storage
+    $(".saveBtn").click(function(){
+        var scheduleInputs = $(this).siblings(".event").val();
+        var inputsLocation = $(this).siblings(".event").attr("id");
+        localStorage.setItem(inputsLocation,scheduleInputs);
+    });
+    
+    setInterval(getDate,1000);
+    colorSchedule();
+    setInterval(colorSchedule,1000);
+    renderStoredInputs();
 
-	/* Adding rows to container div */
-	$(".container").append(row);
-});
-
-/* Color rows based on current time */
-function colorRow(time) {
-	var planNow = moment(now, "H A");
-	var planEntry = moment(time, "H A");
-	if (planNow.isBefore(planEntry) === true) {
-		return "future";
-	} else if (planNow.isAfter(planEntry) === true) {
-		return "past";
-	} else {
-		return "present";
-	}
-}
-
-/* Save Events */
-$(".saveBtn").on("click", function() {
-	var blockID = parseInt(
-		$(this)
-			.closest(".time-block")
-			.attr("id")
-	);
-	var userEntry = $.trim(
-		$(this)
-			.parent()
-			.siblings("textarea")
-			.val()
-	);
-	planWorkday[blockID].event = userEntry;
-
-	/* Set local storage */
-	localStorage.setItem("workDay", JSON.stringify(planWorkday));
 });
